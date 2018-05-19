48c5b82030ba0d10c9407a8b4c490a0d40dc6517
付瑾 <fujin@fujindeMacBook-Pro.local>
2018-05-19 09:59:11 +0800

    解决store报错

--- src/redux/actions/userInfo.js
+++ src/redux/actions/userInfo.js
@@ -1,6 +1,6 @@
-export const GET_USER_INFO_REQUEST = "userinfo/GET_USER_INFO_REQUEST";
-export const GET_USER_INFO_SUCCESS = "userinfo/GET_USER_INFO_SUCCESS";
-export const GET_USER_INFO_FAIL = "userINFO/GET_USER_INFO_FAIL";
+export const GET_USER_INFO_REQUEST = "userInfo/GET_USER_INFO_REQUEST";
+export const GET_USER_INFO_SUCCESS = "userInfo/GET_USER_INFO_SUCCESS";
+export const GET_USER_INFO_FAIL = "userInfo/GET_USER_INFO_FAIL";
 
 function getUserInfoRequest() {
   return {
@@ -8,9 +8,10 @@ function getUserInfoRequest() {
   }
 }
 
-function getUserInfoSuccess() {
+function getUserInfoSuccess(userInfo) {
   return {
-    type: GET_USER_INFO_SUCCESS
+    type: GET_USER_INFO_SUCCESS,
+    userInfo: userInfo
   }
 }
 
--- src/router/router.js
+++ src/router/router.js
@@ -7,6 +7,7 @@ import {
 import Home from 'pages/Home/Home';
 import Page1 from 'pages/Page1/Page1';
 import Counter from 'pages/Counter/Counter';
+import UserInfo from 'pages/UserInfo/UserInfo';
 
 const getRouter = () => {
   return (
@@ -16,10 +17,12 @@ const getRouter = () => {
                 <li><Link to="/">首页</Link></li>
                 <li><Link to="/page1">Page1</Link></li>
                 <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/userinfo">UserInfo</Link></li>
             </ul>
             <Route exact path="/" component={Home}/>
             <Route path="/page1" component={Page1}/>
             <Route path="/counter" component={Counter}></Route>
+            <Route path="/userinfo" component={UserInfo}></Route>
         </div>
     </Router>
   )

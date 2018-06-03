a749cf4a215345bb9a8663986c8fa9680ede3212
付瑾 <fujin@fujindeMacBook-Pro.local>
2018-06-03 15:43:47 +0800

    repire

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,37 @@
-const path = require('path')
-const webpack = require('webpack')
-var HtmlWebpachPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  // 入口
-  devtool: 'cheap-module-source-map',
-  entry: {
-    app: [
-      'react-hot-loader/patch',
-      path.join(__dirname, 'src/index.js')
-    ],
-    vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
-  },
-  /*输出到dist文件夹，输出文件名字为bundle.js*/
+const commonConfig = require('./webpack.common.config.js');
 
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, './dist'),
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[chunkhash].js'
-  },
-  /*src文件夹下面的以.js结尾的文件，要使用babel解析
-  *cacheDirectory是用来缓存编译结果，下次编译加速
-  */
-  module: {
-    rules: [
-      {
-      test: /\.js$/,
-      use: ['babel-loader?cacheDirectory=true'],
-      include: path.join(__dirname, 'src')
-      },
-      {
-        test: /\.css$/,
-        // use: ['style-loader', 'css-loader']
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
-      }, 
-      {
-        test: /\.(png|jpg|gif)/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192  // 小于等于8k的图片会被转成base64编码，直接插入html中，减少http请求
-          }
+const publicConfig = {
+    devtool: 'cheap-module-source-map',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ["css-loader?modules&localIdentName=[local]-[hash:base64:5]", "postcss-loader"]
+            })
         }]
-      }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist/*.*']),
+        new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new ExtractTextPlugin({
+            filename: '[name].[contenthash:5].css',
+            allChunks: true
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpachPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/index.html')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
-    new UglifyJSPlugin(),
-    // 指定环境
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }), 
-    // 优化缓存
-    new webpack.HashedModuleIdsPlugin(),
-    // 打包优化
-    new CleanWebpackPlugin(['dist']),
-    // 抽取css
-    new ExtractTextPlugin({
-      filename: '[name].[contenthash:5].css',
-      allChunks: true
-    })
-  ],
-  
-  resolve: {
-    alias: {
-      pages: path.join(__dirname, 'src/pages'),
-      component: path.join(__dirname, 'src/component'),
-      router: path.join(__dirname, 'src/router'),
-      actions: path.join(__dirname, 'src/redux/actions'),
-      reducers: path.join(__dirname, 'src/redux/reducers')
-    }
-  }
-}
+
+};
+
+module.exports = merge(commonConfig, publicConfig);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,71 +1,45 @@
-const path = require('path')
-const webpack = require('webpack')
-var HtmlWebpachPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  // 入口
-  entry: {
-    app: [
-      'react-hot-loader/patch',
-      path.join(__dirname, 'src/index.js')
-    ],
-    vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
-  },
-  /*输出到dist文件夹，输出文件名字为bundle.js*/
+const commonConfig = require('./webpack.common.config.js');
 
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[chunkhash].js'
-  },
-  /*src文件夹下面的以.js结尾的文件，要使用babel解析
-  *cacheDirectory是用来缓存编译结果，下次编译加速
-  */
-  module: {
-    rules: [
-      {
-      test: /\.js$/,
-      use: ['babel-loader?cacheDirectory=true'],
-      include: path.join(__dirname, 'src')
-      },{
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
-        test: /\.(png|jpg|gif)/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192  // 小于等于8k的图片会被转成base64编码，直接插入html中，减少http请求
-          }
+const devConfig = {
+    devtool: 'inline-source-map',
+    entry: {
+        app: [
+            'babel-polyfill',
+            'react-hot-loader/patch',
+            path.join(__dirname, 'src/index.js')
+        ]
+    },
+    output: {
+        /*这里本来应该是[chunkhash]的，但是由于[chunkhash]和react-hot-loader不兼容。只能妥协*/
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ["style-loader", "css-loader?modules&localIdentName=[local]-[hash:base64:5]", "postcss-loader"]
         }]
-      }
-    ]
-  },
-  devServer: {
-    port: 8080,
-    hot: true,
-    contentBase: path.join(__dirname, './dist'),
-    historyApiFallback: true,
-    host: '0.0.0.0'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpachPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/index.html')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ],
-  
-  resolve: {
-    alias: {
-      pages: path.join(__dirname, 'src/pages'),
-      component: path.join(__dirname, 'src/component'),
-      router: path.join(__dirname, 'src/router'),
-      actions: path.join(__dirname, 'src/redux/actions'),
-      reducers: path.join(__dirname, 'src/redux/reducers')
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.join(__dirname, './dist'),
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        proxy: {
+            "/api/*": "http://localhost:8090/$1"
+        }
     }
-  }
-}
+};
+
+module.exports = merge({
+    customizeArray(a, b, key) {
+        /*entry.app不合并，全替换*/
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
